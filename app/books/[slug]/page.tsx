ddfbd63a4fdd65ee954c0e46bb8cf3fa8f9f6138
Daniel Agg <daniel.agg@outlook.com>
2024-02-19 20:02:12 +0100

    responsivity test

--- app/books/[slug]/page.tsx
+++ app/books/[slug]/page.tsx
@@ -6,14 +6,14 @@ import type { PDFDocumentProxy } from "pdfjs-dist";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const options = {
-  cMapUrl: "/cmaps/",
-  standardFontDataUrl: "/standard_fonts/",
-};
+// const options = {
+//   cMapUrl: "/cmaps/",
+//   standardFontDataUrl: "/standard_fonts/",
+// };
 
 export default function Book({ params }: { params: { slug: string } }) {
   const [numPages, setNumPages] = useState<number>();
-
+  const [pageNumber, setPageNumber] = useState<number>(1);
   // page: 58
 
   function onDocumentLoadSuccess({
@@ -23,15 +23,35 @@ export default function Book({ params }: { params: { slug: string } }) {
   }
 
   return (
-    <main className="min-w-[100vw] min-h-[100vh] bg-red-200 text-4xl">
-      <div>My Post: {params.slug}</div>
+    <main className="min-w-[100vw] min-h-[100vh] flex flex-col items-center justify-center">
       <Document
+        className="border w-2/3 h-[90vh] overflow-y-scroll hidden lg:block"
         file="/books/pragmatic-programmer.pdf"
         onLoadSuccess={onDocumentLoadSuccess}
-        options={options}
       >
         {Array.from(new Array(numPages), (el, index) => (
-          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={58}
+            width={1200}
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+          />
+        ))}
+      </Document>
+      <Document
+        className="border w-full h-[90vh] overflow-y-scroll block lg:hidden"
+        file="/books/pragmatic-programmer.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={58}
+            width={600}
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+          />
         ))}
       </Document>
     </main>
--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { PT_Serif } from "next/font/google";
+import { Skeleton } from "@/components/ui/skeleton";
+import Link from "next/link";
 
 const ptSerif = PT_Serif({ weight: ["700"], subsets: ["latin"] });
 
@@ -15,31 +17,36 @@ export default function Home() {
   ];
   return (
     <main className="flex min-h-screen flex-col justify-start items-center p-4 lg:p-24">
-      <section className="flex flex-col w-full lg:w-2/3">
+      <section className="flex flex-col w-full lg:w-3/4">
         <h1
           className={`text-4xl lg:text-6xl text-gray-400 font-bold text-center mt-12 lg:mt-0 ${ptSerif.className}`}
         >
           My Books
         </h1>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-12">
-          {[...books, ...books, ...books].map((book) => {
-            return (
-              <div
-                className="rounded-3xl p-4 lg:py-16 bg-gray-300 text-slate-900 flex flex-col items-center justify-center text-center"
-                key={book.title}
-              >
-                <h1
-                  className={`font-black text-xl lg:text-2xl ${ptSerif.className}`}
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-12">
+          {[...books, ...books, ...books, ...books, ...books, ...books].map(
+            (book) => {
+              return (
+                <div
+                  className="rounded-3xl py-4 bg-gray-100 text-slate-900 flex flex-col items-center justify-center text-center"
+                  key={book.title}
                 >
-                  {book.title}
-                </h1>
-                <p className="pt-1 opacity-75">{book.author}</p>
+                  <Skeleton className="h-[24rem] w-[90%] mt-4 mb-12 rounded-xl" />
+                  <h1
+                    className={`font-black text-xl lg:text-2xl ${ptSerif.className}`}
+                  >
+                    {book.title}
+                  </h1>
+                  <p className="pt-1 opacity-75">{book.author}</p>
 
-                <Button className="mt-6">Continue Reading</Button>
-              </div>
-            );
-          })}
+                  <Button className="mt-6">
+                    <Link href={`/books/${book.slug}`}>Continue Reading</Link>
+                  </Button>
+                </div>
+              );
+            }
+          )}
         </div>
       </section>
     </main>
