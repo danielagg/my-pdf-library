de5e9d2dd081f454af79d12aee36459cc96753e9
Daniel Agg <daniel.agg@outlook.com>
2024-04-07 21:17:06 +0200

    manual token refresh

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,4 +1,4 @@
-import { AuthOptions } from "next-auth";
+import { AuthOptions, TokenSet } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: AuthOptions = {
@@ -16,12 +16,39 @@ export const authOptions: AuthOptions = {
       },
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async jwt({ account, token }) {
       if (account?.access_token) {
         token.access_token = account.access_token;
         token.refresh_token = account.refresh_token;
+
+        token.expires_at = Math.floor(Date.now() / 1060);
+
+        if (Date.now() > (token.expires_at as any) * 1000) {
+          const response = await fetch("https://oauth2.googleapis.com/token", {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams({
+              client_id: "" + process.env.GOOGLE_CLIENT_ID,
+              client_secret: "" + process.env.GOOGLE_CLIENT_SECRET,
+              grant_type: "refresh_token",
+              refresh_token: "" + token.refresh_token,
+            }),
+            method: "POST",
+          });
+
+          const tokens: TokenSet = await response.json();
+
+          if (!response.ok) throw tokens;
+
+          token.access_token = tokens.access_token as string;
+          token.expires_at = Math.floor(Date.now() / 1060);
+
+          return token;
+        }
       }
 
       return token;
